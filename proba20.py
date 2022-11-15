2d1d0500dc7fb015bb64eb08599d01c511f5597d
Viktorius98 <viksin376@yandex.ru>
2022-11-15 23:29:27 +0300

    commit files 11

--- proba20.py
+++ proba20.py
@@ -16,3 +16,34 @@ from decorators import do_twice
 @do_twice
 def test_twice():
     print("Это вызов функции test_twice!")
+
+def do_twice(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+    return wrapper
+
+@do_twice
+def test_twice_without_params():
+    print("Этот вызов без параметров")
+
+
+@do_twice
+def test_twice_2_params(str1, str2):
+    print("В этой функции 2 параметра - {0} и {1}".format(str1, str2))
+
+@do_twice
+def test_twice(str):
+    print("Этот вызов возвращает строку {0}".format(str))
+
+test_twice_without_params()
+test_twice_2_params("1", "2")
+test_twice("single")
+
+@do_twice
+def test_twice(str):
+    print("Этот вызов возвращает строку {0}".format(str))
+    return "Done"
+
+decorated_value = test_twice("single")
+print(decorated_value)
