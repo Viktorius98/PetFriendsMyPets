3f9af8abaf538c87ddd8358f71853d3bf72b8bde
Viktorius98 <viksin376@yandex.ru>
2022-12-20 23:47:35 +0300

    commit files 18

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -4,11 +4,11 @@ import time
 
 def test_auth_page(selenium):
    page = AuthPage(selenium)
-   page.enter_email("viksin376@yandex.ru")
-   page.enter_pass("Rkb[Ntc23")
+   page.enter_email("email@gmail.com")
+   page.enter_pass("pass")
    page.btn_click()
 
    #знак != или == будет зависеть от того, верные или неверные данные мы вводим
-    assert page.get_relative_link() != '/all_pets', "login error"
+   assert page.get_relative_link() != '/all_pets', "login error"
 
    time.sleep(5) #задержка для учебных целей
