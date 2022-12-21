e67438459c84953016a61c125a957c9e8ec23068
Viktorius98 <viksin376@yandex.ru>
2022-12-21 15:04:30 +0300

    commit files 18

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,14 +1,27 @@
-from pages.auth_page import AuthPage
-import time
+from .base_page import BasePage
+from .locators import AuthLocators
+
+import  time,os
 
 
-def test_auth_page(selenium):
-   page = AuthPage(selenium)
-   page.enter_email("email@gmail.com")
-   page.enter_pass("pass")
-   page.btn_click()
+class AuthPage(BasePage):
 
-   #знак != или == будет зависеть от того, верные или неверные данные мы вводим
-   assert page.get_relative_link() != '/all_pets', "login error"
+   def __init__(self, driver,timeout=10):
+       super().__init__(driver, timeout)
+       url = os.getenv("LOGIN_URL") or "http://petfriends.skillfactory.ru/login"
+driver.get(url)
+#создаем нужные элементы
+       self.email = driver.find_element(*AuthLocators.AUTH_EMAIL)
+       self.password = driver.find_element(*AuthLocators.AUTH_PASS)
+       self.btn = driver.find_element(*AuthLocators.AUTH_BTN)
+       time.sleep(3)
 
-   time.sleep(5) #задержка для учебных целей
+   def enter_email(self, value):
+       self.email.send_keys(value)
+
+   def enter_pass(self, value):
+       self.password.send_keys(value)
+
+   def btn_click(self):
+       self.btn.click()
+  time.sleep(3) #ждем реакции
